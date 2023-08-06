e9e2ffcb3118b63684dec2012396e9c7f9aef547
Soocyang <soociayang_99@hotmail.com>
2023-08-06 14:52:26 +0800

    initialize

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <nuxt-link to="/">Logo</nuxt-link>
+      </nav>
+    </header>
+    <div >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi temporibus consequuntur sint debitis nostrum
+      porro, beatae sit? Unde consectetur quas nulla minima harum, odit voluptatibus placeat eum similique autem quae!</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+
+}
+
+p {
+  margin: 20 0;
+}
+</style>
