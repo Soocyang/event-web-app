c12797fb27bca02b266dd64a43fc6b3ce63b0b9d
Soocyang <soociayang_99@hotmail.com>
2023-08-06 17:52:49 +0800

    add header nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <nuxt-link to="/">Logo</nuxt-link>
+    <header class="sticky top-0 z-10 w-full backdrop-blur shadow-lg bg-white/95 text-primary">
+      <nav class="h-12 sm:h-16 flex items-center justify-center text-2xl">
+        <nuxt-link to="/" class="flex items-center justify-center">
+          <img src="/favicon.ico" alt="logo icon" class="w-7 mx-2">
+          <span class="font-semibold text-accent">Gi</span>
+          <span class="font-normal ">mon</span>
+        </nuxt-link>
       </nav>
     </header>
-    <div >
+    <div class="h-[1000px]">
       <slot />
     </div>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
