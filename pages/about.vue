0df86c0206981e4eeb09c95af0749066716ae621
Soocyang <soociayang_99@hotmail.com>
2023-08-06 20:29:30 +0800

    setup bottom nav

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2 class="text-4xl p-2">Home</h2>
+    <h2 class="text-4xl p-2">About</h2>
     <p class="p-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi temporibus consequuntur sint debitis nostrum
       porro, beatae sit? Unde consectetur quas nulla minima harum, odit voluptatibus placeat eum similique autem quae!</p>
   </div>
