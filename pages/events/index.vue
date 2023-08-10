913264d9a680e13a70468b217efbfdd0d50d51f4
Soocyang <soociayang_99@hotmail.com>
2023-08-10 23:00:03 +0800

    responsive events listing

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid place-content-center px-3 pt-5 pb-20">
+  <div class="max-w-5xl m-auto px-3 pt-5 pb-20">
     <h1 class="px-3 text-2xl font-bold text-gradient-primary">Upcoming Events</h1>
     <EventListing :eventListing="eventListing" :editable="false" />
   </div>
